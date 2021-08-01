bf03c66f5dd6e335dae8e4ff9acc95f5e3388776
Anton G Neuhold Jr <aneuhold@predictiveindex.com>
2021-08-01 12:30:41 -0700

    Add basic inventory item model

---
+++ backend/PersonalInventoryAPI/Models/IInventoryItem.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace PersonalInventoryAPI.Models {
+  interface IInventoryItem {
+    public Guid Id { get; }
+    public string Name { get; set; }
+    public string Location { get; set; }
+
+  }
+}
---
+++ backend/PersonalInventoryAPI/Models/InventoryItem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace PersonalInventoryAPI.Models {
+  public class InventoryItem : IInventoryItem {
+    public Guid Id { get; }
+    public string Name { get; set; }
+    public string Location { get; set; }
+
+    public InventoryItem(string name, string location) : this() {
+      Name = name;
+      Location = location;
+    }
+
+    public InventoryItem(string name) : this() {
+      Name = name;
+    }
+
+    public InventoryItem() {
+      Id = new Guid();
+    }
+  }
+}
