ba95300c698f667534fe0233a9a24cd4205bd9e6
Anton G Neuhold Jr <aneuhold@predictiveindex.com>
2021-08-01 11:57:47 -0700

    Add basic backend code

---
+++ backend/PersonalInventoryAPI.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PersonalInventoryAPI.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
