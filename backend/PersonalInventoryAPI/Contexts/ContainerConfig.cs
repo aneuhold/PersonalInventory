730bf3d82048806b203c0d0a164705b9b0e6bd8a
Anton G Neuhold Jr <aneuhold@predictiveindex.com>
2021-08-06 07:59:22 -0700

    Update ContainerConfig comment

--- backend/PersonalInventoryAPI/Contexts/ContainerConfig.cs
+++ backend/PersonalInventoryAPI/Contexts/ContainerConfig.cs
@@ -3,6 +3,10 @@ using PersonalInventoryAPI.Models;
 using PersonalInventoryAPI.Models.Interfaces;
 
 namespace PersonalInventoryAPI.Contexts {
+
+  /// <summary>
+  /// This currently isn't used, but will be when Autofac is used.
+  /// </summary>
   public class ContainerConfig {
     public static IContainer Configure() {
       var builder = new ContainerBuilder();
